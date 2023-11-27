ae03bb3d481385b53d70bd6fa9456d5e7294f362
Samuel Seven <sevenofice777@gmail.com>
2023-11-27 10:08:03 -0300

    style(buttons): add styles buttons

--- Desafio Final/Form1.Designer.cs
+++ Desafio Final/Form1.Designer.cs
@@ -29,34 +29,35 @@ namespace Desafio_Final
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.Btn_login = new System.Windows.Forms.Button();
+            this.Btn_signup = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button1
+            // Btn_login
             // 
-            this.button1.Location = new System.Drawing.Point(288, 264);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(116, 37);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "LOGIN";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.Btn_login.Location = new System.Drawing.Point(242, 225);
+            this.Btn_login.Name = "Btn_login";
+            this.Btn_login.Size = new System.Drawing.Size(116, 37);
+            this.Btn_login.TabIndex = 0;
+            this.Btn_login.Text = "Login";
+            this.Btn_login.UseVisualStyleBackColor = false;
+            this.Btn_login.Click += new System.EventHandler(this.Btn_login_Click);
             // 
-            // button2
+            // Btn_signup
             // 
-            this.button2.Location = new System.Drawing.Point(426, 264);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(146, 37);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "CADASTRAR";
-            this.button2.UseVisualStyleBackColor = true;
+            this.Btn_signup.Location = new System.Drawing.Point(380, 225);
+            this.Btn_signup.Name = "Btn_signup";
+            this.Btn_signup.Size = new System.Drawing.Size(146, 37);
+            this.Btn_signup.TabIndex = 1;
+            this.Btn_signup.Text = "Cadastrar";
+            this.Btn_signup.UseVisualStyleBackColor = false;
+            this.Btn_signup.Click += new System.EventHandler(this.Btn_signup_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(385, 172);
+            this.label1.Location = new System.Drawing.Point(339, 133);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(72, 13);
             this.label1.TabIndex = 2;
@@ -67,10 +68,10 @@ namespace Desafio_Final
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Desafio_Final.Properties.Resources.background_container1;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(750, 420);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Btn_signup);
+            this.Controls.Add(this.Btn_login);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -80,8 +81,8 @@ namespace Desafio_Final
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button Btn_login;
+        private System.Windows.Forms.Button Btn_signup;
         private System.Windows.Forms.Label label1;
     }
 }
--- Desafio Final/Form1.cs
+++ Desafio Final/Form1.cs
@@ -1,25 +1,65 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing.Text;
 using System.Windows.Forms;
 
 namespace Desafio_Final
 {
     public partial class Form1 : Form
     {
+        PrivateFontCollection privateFontCollection = new PrivateFontCollection();
+
         public Form1()
         {
             InitializeComponent();
+            CarregarFonte();
+            StyleButton(Btn_login);
+            StyleButton(Btn_signup);
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void CarregarFonte()
         {
+            // Substitua "MinhaFonte" pelo nome real do arquivo da sua fonte (sem extensão)
+            privateFontCollection.AddFontFile("C:/Users/Aluno/source/repos/Desafio-Final/Desafio Final/assets/CaviarDreams.ttf");
+        }
 
+        public void StyleButton(Button button)
+        {
+            button.MouseEnter += (sender, e) =>
+            {
+                // Altera a cor do fundo quando o mouse entra no botão
+                button.BackColor = Color.FromArgb(138, 10, 10);
+                button.Cursor = Cursors.Hand;
+            };
+
+            button.MouseLeave += (sender, e) =>
+            {
+                // Retorna à cor original quando o mouse deixa o botão
+                button.BackColor = Color.Transparent; // Ou a cor original que você definiu
+            };
+            // Use o índice 0 para acessar a fonte carregada pela PrivateFontCollection
+            Font minhaFonte = new Font(privateFontCollection.Families[0], 12, FontStyle.Regular);
+
+            // Atribua a fonte ao botão
+            button.Font = minhaFonte;
+
+            // Adicione outras configurações de estilo conforme necessário
+            button.BackColor = Color.Transparent;
+            button.ForeColor = Color.White;
+            button.FlatStyle = FlatStyle.Flat;
+            button.FlatAppearance.BorderSize = 1;
+            button.FlatAppearance.BorderColor = Color.White;
+            button.Padding = new Padding(10, 5, 10, 5);
+        }
+
+        private void Btn_login_Click(object sender, EventArgs e)
+        {
+            // Lógica para o clique no botão de login
+        }
+
+        private void Btn_signup_Click(object sender, EventArgs e)
+        {
+            // Lógica para o clique no botão de signup
         }
     }
 }
