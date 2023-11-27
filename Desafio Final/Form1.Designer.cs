3e5d42fb8dff7b74437105c42d9c65856a729e85
Samuel Seven <sevenofice777@gmail.com>
2023-11-27 07:33:28 -0300

    style(bg):add bg imagem

--- Desafio Final/Form1.Designer.cs
+++ Desafio Final/Form1.Designer.cs
@@ -29,10 +29,18 @@ namespace Desafio_Final
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Desafio_Final.Properties.Resources.background_container;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
