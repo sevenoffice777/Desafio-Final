629bc3f17a74684342eba6fe60ea744e12ef60c5
Samuel Seven <sevenofice777@gmail.com>
2023-11-27 07:44:02 -0300

    style(bg): alter background

--- Desafio Final/Form1.Designer.cs
+++ Desafio Final/Form1.Designer.cs
@@ -35,7 +35,7 @@ namespace Desafio_Final
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = global::Desafio_Final.Properties.Resources.background_container;
+            this.BackgroundImage = global::Desafio_Final.Properties.Resources.background_container1;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Name = "Form1";
             this.Text = "Form1";
--- Desafio Final/Properties/Resources.Designer.cs
+++ Desafio Final/Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace Desafio_Final.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap background_container1 {
+            get {
+                object obj = ResourceManager.GetObject("background_container1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
