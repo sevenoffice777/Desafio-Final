676a80fa5811a1e5328777b9952187c93d800b76
Samuel Seven <sevenofice777@gmail.com>
2023-11-27 13:47:24 -0300

    feat(login_view): add login view form

--- Desafio Final/Form2.Designer.cs
+++ Desafio Final/Form2.Designer.cs
@@ -30,7 +30,91 @@ namespace Desafio_Final
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.userTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(158, 163);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Login";
+            
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(156, 216);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(228, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Informe suas Credenciais para entrar na 7Code";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.passwordTextBox);
+            this.groupBox1.Controls.Add(this.userTextBox);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.groupBox1.Location = new System.Drawing.Point(432, 109);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(337, 191);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Login";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(432, 306);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(337, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Entrar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 38);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Usuário";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 107);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Senha";
+            // 
+            // userTextBox
+            // 
+            this.userTextBox.Location = new System.Drawing.Point(7, 66);
+            this.userTextBox.Name = "userTextBox";
+            this.userTextBox.Size = new System.Drawing.Size(324, 20);
+            this.userTextBox.TabIndex = 4;
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(7, 142);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(324, 20);
+            this.passwordTextBox.TabIndex = 5;
             // 
             // Form2
             // 
@@ -38,14 +122,29 @@ namespace Desafio_Final
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Desafio_Final.Properties.Resources.background_container2;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form2";
             this.Text = "7code Hub - Login";
-            this.Load += new System.EventHandler(this.Form2_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.TextBox userTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
--- Desafio Final/Form2.cs
+++ Desafio Final/Form2.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Text;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,14 +13,76 @@ namespace Desafio_Final
 {
     public partial class Form2 : Form
     {
+         PrivateFontCollection privateFontCollection = new PrivateFontCollection();
         public Form2()
         {
             InitializeComponent();
+            CarregarFonte();
+            StyleButton(button1);
+            StyleTitle(label1);
+            StyleText(label2);
         }
 
-        private void Form2_Load(object sender, EventArgs e)
+
+        private void CarregarFonte()
         {
+            // Substitua "MinhaFonte" pelo nome real do arquivo da sua fonte (sem extensão)
+            privateFontCollection.AddFontFile("C:/Users/Aluno/source/repos/Desafio-Final/Desafio Final/assets/CaviarDreams.ttf");
+            // Passivo de mudança DEPENDENDO DO USUARIO E CAMINHO
 
         }
+
+
+        private void StyleText(Label labelText)
+        {
+            Font minhaFonte = new Font(privateFontCollection.Families[0], 12, FontStyle.Regular);
+            labelText.Location = new System.Drawing.Point(50, 256);
+            labelText.Font = minhaFonte;
+            labelText.ForeColor = Color.White;
+            labelText.BackColor = Color.Transparent;
+        }
+
+
+        private void StyleTitle(Label labelTitle)
+        {
+            Font minhaFonte = new Font(privateFontCollection.Families[0], 40, FontStyle.Regular);
+
+            labelTitle.Font = minhaFonte;
+            labelTitle.ForeColor = Color.White;
+            labelTitle.BackColor = Color.Transparent;
+        }
+
+        public void StyleButton(Button button)
+        {
+            button.MouseEnter += (sender, e) =>
+            {
+                // Altera a cor do fundo quando o mouse entra no botão
+                button.FlatAppearance.BorderColor = Color.Red;
+                button.BackColor = Color.Red;
+                button.Cursor = Cursors.Hand;
+            };
+
+            button.MouseLeave += (sender, e) =>
+            {
+                button.FlatAppearance.BorderColor = Color.FromArgb(138, 10, 10);
+                // Retorna à cor original quando o mouse deixa o botão
+                button.BackColor = Color.FromArgb(138, 10, 10); // Ou a cor original que você definiu
+            };
+            // Use o índice 0 para acessar a fonte carregada pela PrivateFontCollection
+            Font minhaFonte = new Font(privateFontCollection.Families[0], 12, FontStyle.Regular);
+
+            // Atribua a fonte ao botão
+            button.Font = minhaFonte;
+
+            // Adicione outras configurações de estilo conforme necessário
+            button.BackColor = Color.Red;
+            button.ForeColor = Color.White;
+            button.FlatStyle = FlatStyle.Flat;
+            button.FlatAppearance.BorderSize = 1;
+            button.FlatAppearance.BorderColor = Color.Red;
+            button.Padding = new Padding(2);
+        }
+
+        
     }
 }
